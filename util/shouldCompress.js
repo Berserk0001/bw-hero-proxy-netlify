ebdacdeeb9c3661b57e40737a87ac4660bd803cf
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-06-26 21:56:46 +0530

    Update shouldCompress.js

--- util/shouldCompress.js
+++ util/shouldCompress.js
@@ -2,14 +2,15 @@ const MIN_COMPRESS_LENGTH = 512; // ~0.5Kb
 const MIN_TRANSPARENT_COMPRESS_LENGTH = MIN_COMPRESS_LENGTH * 2; // ~10KB
 
 function shouldCompress(originType, originSize, webp) {
-	if (!originType.startsWith("image")) return true;
+	if (!originType.startsWith("image")) return false;
 	if (originSize === 0) return false;
-	if (webp && originSize < MIN_COMPRESS_LENGTH) return true;
-	if (	// if png or gif image(also shouldn't be webp)
+	if (webp && originSize < MIN_COMPRESS_LENGTH) return false;
+	if (	// if png or gif image, and size is less than compression limit (also shouldn't be webp)
 		!webp &&
-		(originType.endsWith("png") || originType.endsWith("gif"))
+		(originType.endsWith("png") || originType.endsWith("gif")) &&
+		originSize < MIN_TRANSPARENT_COMPRESS_LENGTH
 	) {
-		return true;
+		return false;
 	}
 
 	return true;
